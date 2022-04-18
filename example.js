932c8aca7bfc81b434e87862223511ac33b2771b
Jannis R <mail@jannisr.de>
2022-04-18 23:48:57 +0200

    example script 📝

--- example.js
+++ example.js
@@ -1,3 +1,27 @@
 'use strict'
 
-// todo
+import pino from 'pino'
+import {createClient} from './index.js'
+
+const logger = pino({
+	level: process.env.LOG_LEVEL || 'info',
+})
+
+const {
+	httpServer,
+} = createClient({
+	leitstelle: 'vdv-453-client-example',
+	endpoint: 'https://example.org/', // todo
+	logger,
+})
+
+httpServer.listen(3000, (err) => {
+	if (err) {
+		logger.error({
+			error: err,
+		}, err.message)
+		process.exit(1)
+	}
+
+	logger.info('listening on port 3000')
+})
