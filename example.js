4f32847d882d0e8a0879ffcdcac23773828a4436
Jannis R <mail@jannisr.de>
2024-03-12 21:42:46 +0100

    example: make leitstelle ID & port configurable 📝

--- example.js
+++ example.js
@@ -3,13 +3,31 @@
 import {promisify} from 'node:util'
 import {createClient} from './index.js'
 
+const abortWithError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+if (!process.env.VDV_453_LEITSTELLE) {
+	abortWithError('missing/empty $VDV_453_LEITSTELLE')
+}
+const LEITSTELLE = process.env.VDV_453_LEITSTELLE
+if (!process.env.VDV_453_ENDPOINT) {
+	abortWithError('missing/empty $VDV_453_ENDPOINT')
+}
+const ENDPOINT = process.env.VDV_453_ENDPOINT
+if (!process.env.PORT) {
+	abortWithError('missing/empty $PORT')
+}
+const PORT = process.env.PORT
+
 const {
 	logger,
 	httpServer,
 } = createClient({
-	leitstelle: 'vdv-453-client-example',
-	endpoint: 'https://example.org/', // todo
+	leitstelle: LEITSTELLE,
+	endpoint: ENDPOINT,
 })
 
-await promisify(httpServer.listen.bind(httpServer))(3000)
-logger.info('listening on port 3000')
+await promisify(httpServer.listen.bind(httpServer))(PORT)
+logger.info(`listening on port ${PORT}`)
