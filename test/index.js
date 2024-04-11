dc80da988a16c93de84de6591c5fcd41e66eef6b
Jannis R <mail@jannisr.de>
2024-04-12 00:09:22 +0200

    XML parsing: add test ✅

--- test/index.js
+++ test/index.js
@@ -1,9 +1,36 @@
 'use strict'
 
 import {test} from 'tape'
+import {readFileSync} from 'node:fs'
+import {Readable} from 'node:stream'
+import {strictEqual} from 'node:assert'
+import {parseTags} from '../lib/xml-parser.js'
 import {runClient} from './util.js'
 
 const LEITSTELLE = 'test-client'
+
+const VBB_DDS_AUS_DATENABRUFENANTWORT_2024_04_11 = readFileSync(
+	new URL('./vbb-dds-aus-datenabrufenantwort-2024-04-11.xml', import.meta.url).pathname,
+)
+
+test('XML parsing works', async (t) => {
+	const input = Readable.from(VBB_DDS_AUS_DATENABRUFENANTWORT_2024_04_11)
+	const parser = parseTags(input, [
+		{tag: 'Bestaetigung', preserve: true},
+		{tag: 'WeitereDaten', preserve: true},
+		{tag: 'AUSNachricht', preserve: true},
+	])
+
+	const els = []
+	for await (const [_, el] of parser) {
+		els.push(el)
+	}
+
+	strictEqual(els[0].$name, 'Bestaetigung', 'els[0].$name')
+	strictEqual(els[1].$name, 'WeitereDaten', 'els[1].$name')
+	strictEqual(els[2].$name, 'AUSNachricht', 'els[2].$name')
+	strictEqual(els[2].children.length, 2, 'els[2].children.length')
+})
 
 test('todo', async (t) => {
 	const {
