314fa0e9c65ac9c0cd86e688237742c3cd324c57
Jannis R <mail@jannisr.de>
2025-11-16 19:09:57 +0100

    eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			globals: globals.node,
+		},
+		rules: {
+			'no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					args: 'none',
+					ignoreRestSiblings: false,
+				},
+			],
+		},
+	},
+]
